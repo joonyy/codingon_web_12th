5cfecf3a2e57f3eeb40503ddfe8031c5545071d4
Joonyoung <blackhound97@gmail.com>
2024-03-27 12:04:42 +0900

    임시 저장 : express + ejs 사용법

---
+++ LECTURE/07_Node/14/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req,res)=>{
+  res.render('index', {title:"my Home"})
+})
+app.get('/about', (req,res)=>{
+  res.render('index', {title:"my about"})
+})
+app.get('/create', (req,res)=>{
+  res.render('index', {title:"my create"})
+})
+app.use((req,res)=>{
+  res.status(404).render('404');
+})
+
+app.listen(8080, function(){
+  console.log('8080번 포트에서 서버 실행중');
+});
